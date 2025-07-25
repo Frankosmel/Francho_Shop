5f0ffd447f1e3ca1faf30cf52a2a8d9c78fae07a
Frankosmel <130179322+Frankosmel@users.noreply.github.com>
2025-07-25 14:18:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
 # app.py
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
 
-# Inicializar app y configuración
+# Importamos db pendiente de inicializar
+from models import db, User, Recarga
+
+# Inicializamos la app
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Base de datos
-db = SQLAlchemy(app)
+# Inicializamos la base de datos con la app
+db.init_app(app)
 
-# Login Manager
+# Login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 
-# Importar modelos
-from models import User
-
-# Cargar usuario logueado
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Registrar blueprints
+# Registrar Blueprints
 from routes.auth import auth_bp
 from routes.user import user_bp
 from routes.admin import admin_bp
@@ -34,10 +32,9 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(admin_bp)
 
-# Crear DB automáticamente si no existe
+# Crear tablas si no existen
 with app.app_context():
     db.create_all()
 
-# Ejecutar app
 if __name__ == '__main__':
     app.run(debug=True)
